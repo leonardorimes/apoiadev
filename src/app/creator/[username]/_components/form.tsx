16ec351064839f26794983f91c4872998f6a7e59
Leonardo Rimes <rimesleo@gmail.com>
2025-09-16 18:34:10 -0300

    criando codigos

--- src/app/creator/[username]/_components/form.tsx
+++ src/app/creator/[username]/_components/form.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Nome obrigatório" }),
+  message: z.string().min(1, { message: "Mensagem obrigatória" }),
+  price,
+});
+
 export function FormDonate() {
   return <div>form</div>;
 }
