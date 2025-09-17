e47eae90fa6e016a686998eec7d1b4cbc1ec4f89
Leonardo Rimes <rimesleo@gmail.com>
2025-09-17 18:36:32 -0300

    criando formulario com shadon ui

--- src/app/creator/[username]/_components/form.tsx
+++ src/app/creator/[username]/_components/form.tsx
@@ -4,12 +4,135 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+
 const formSchema = z.object({
-  name: z.string().min(1, { message: "Nome obrigatório" }),
-  message: z.string().min(1, { message: "Mensagem obrigatória" }),
-  price,
+  name: z.string().min(1, "Nome obrigatório"),
+  message: z.string().min(1, "Mensagem obrigatória"),
+  price: z.enum(["15", "25", "35"], {
+    required_error: "O valor é obrigatório",
+    invalid_type_error: "O valor é obrigatório",
+  }),
 });
 
+type FormData = z.infer<typeof formSchema>;
+
 export function FormDonate() {
-  return <div>form</div>;
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      message: "",
+      price: "15",
+    },
+  });
+
+  function onSubmit(values: FormData) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8 mt-5" onSubmit={form.handleSubmit(onSubmit)}>
+        {/* Nome */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Digite seu nome"
+                  {...field}
+                  className="bg-white"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Mensagem */}
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mensagem</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Digite sua mensagem"
+                  {...field}
+                  className="bg-white h-32 resize-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Mensagem */}
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mensagem</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Digite sua mensagem"
+                  {...field}
+                  className="bg-white h-32 resize-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Preço */}
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Valor da doação</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex items-center gap-3"
+                >
+                  {["15", "25", "35"].map((value) => (
+                    <div key={value} className="flex items-center gap-1">
+                      <RadioGroupItem value={value} id={value} />
+                      <label className="text-lg " htmlFor={value}>
+                        R$ {value}
+                      </label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Fazer doação</Button>
+      </form>
+    </Form>
+  );
 }
